3eb4c404316be88d50ce02f66360e26c58d7ea98
John Wohlbier <jgwohlbier@sei.cmu.edu>
2020-01-23 11:17:35 -0500

    updates to training.

--- object_detection/run_and_time.sh
+++ object_detection/run_and_time.sh
@@ -5,7 +5,9 @@
 pushd pytorch
 
 # Single GPU training
-time python tools/train_mlperf.py --config-file "configs/e2e_mask_rcnn_R_50_FPN_1x.yaml" \
+export NGPUS=8
+DIST="-m torch.distributed.launch --nproc_per_node=$NGPUS"
+time python ${DIST} tools/train_mlperf.py --config-file "configs/e2e_mask_rcnn_R_50_FPN_1x.yaml" \
        SOLVER.IMS_PER_BATCH 2 TEST.IMS_PER_BATCH 1 SOLVER.MAX_ITER 720000 SOLVER.STEPS "(480000, 640000)" SOLVER.BASE_LR 0.0025
-       
+
 popd
