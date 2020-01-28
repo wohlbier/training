cd8d16cd0608e40c3b775a98627e8e36a1a1b412
John Wohlbier <jgwohlbier@sei.cmu.edu>
2020-01-27 19:15:39 -0500

    make 4 gpus default

--- object_detection/run_and_time.sh
+++ object_detection/run_and_time.sh
@@ -5,9 +5,9 @@
 pushd pytorch
 
 # Single GPU training
-#export NGPUS=8
-#DIST="-m torch.distributed.launch --nproc_per_node=$NGPUS"
+export NGPUS=4
+DIST="-m torch.distributed.launch --nproc_per_node=$NGPUS"
 time python ${DIST} tools/train_mlperf.py --config-file "configs/e2e_mask_rcnn_R_50_FPN_1x.yaml" \
-       SOLVER.IMS_PER_BATCH 2 TEST.IMS_PER_BATCH 1 SOLVER.MAX_ITER 720000 SOLVER.STEPS "(480000, 640000)" SOLVER.BASE_LR 0.0025
+       SOLVER.IMS_PER_BATCH ${NGPUS} TEST.IMS_PER_BATCH ${NGPUS} SOLVER.MAX_ITER 720000 SOLVER.STEPS "(480000, 640000)" SOLVER.BASE_LR 0.0025
 
 popd
